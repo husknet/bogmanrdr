9f9c2a8c37f575abb0395e73f161ba971e264e12
husknet <127146284+husknet@users.noreply.github.com>
2024-08-05 13:01:52 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://blow2guts.vercel.app/';
+            window.location.href = 'https://blow2guts-ts2v.vercel.app/';
         }, 2000);
 
         return () => clearTimeout(timer);
